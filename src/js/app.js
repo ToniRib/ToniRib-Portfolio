6ffaf1731e4f0e6ec91932bede2e1123a200676b
ToniRib <tonimarierib@gmail.com>
2015-09-30 21:01:12 -0600

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+// $(document).ready(function() {
+// 	$('a').mouseenter(function() {
+// 		$(this).fadeTo('fast', 0.5);
+// 	});
+
+// 	$('a').mouseleave(function() {
+// 		$(this).fadeTo('fast', 1);
+// 	});
+// });
+
+
+// $(document).ready(function() {
+// 	$('#bioLink').click(function() {
+// 		$('body').animate({
+// 		    'scrollTop':   $('#anchorBio').offset().top
+// 		}, 2000);
+// 	});
+// });
+
+
+
