3ee958a5d55efacc04f65be57aa1a41317e56e0d
ToniRib <tonimarierib@gmail.com>
2015-09-30 22:04:03 -0600

    Updates biography section with text and rocket svg image

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,27 @@
-// $(document).ready(function() {
-// 	$('a').mouseenter(function() {
-// 		$(this).fadeTo('fast', 0.5);
-// 	});
+// nav link color fade
+$(document).ready(function() {
+	$('a').mouseenter(function() {
+		$(this).fadeTo('fast', 0.5);
+	});
 
-// 	$('a').mouseleave(function() {
-// 		$(this).fadeTo('fast', 1);
-// 	});
-// });
+	$('a').mouseleave(function() {
+		$(this).fadeTo('fast', 1);
+	});
+});
 
-
-// $(document).ready(function() {
-// 	$('#bioLink').click(function() {
-// 		$('body').animate({
-// 		    'scrollTop':   $('#anchorBio').offset().top
-// 		}, 2000);
-// 	});
-// });
-
-
-
+// smooth scrolling to anchors
+// Credit to Chris Coyier: https://css-tricks.com/snippets/jquery/smooth-scrolling/
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
